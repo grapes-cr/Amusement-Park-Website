851ac96af0c3c3440952cf95651bd1de8cab40c9
Brendan Beagin <brendanpatrickbeagin@gmail.com>
2021-12-11 23:26:28 -0800

    Refactored Project to build entirely with maven!

--- src/main/java/com/csun/RobotDevTeamWorld/servlet/RobotDevTeamWorld.java
+++ src/main/java/com/csun/RobotDevTeamWorld/servlet/RobotDevTeamWorld.java
@@ -1,6 +1,7 @@
-
+package com.csun.RobotDevTeamWorld.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,32 +9,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class FirstServlet
+ * Servlet implementation class RobotDevTeamWorld
  */
-@WebServlet("/FirstServlet")
-public class FirstServlet extends HttpServlet {
+@WebServlet("/RobotDevTeamWorld")
+public class RobotDevTeamWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
-     * Default constructor. 
+     * @see HttpServlet#HttpServlet()
      */
-    public FirstServlet() {
-        // TODO Auto-generated constructor stub
+    public RobotDevTeamWorld() {
+        super();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.sendRedirect(request.getContextPath()); //Pulls up the welcome page, Welcome.jsp.
+
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		doGet(request, response);
 	}
 
