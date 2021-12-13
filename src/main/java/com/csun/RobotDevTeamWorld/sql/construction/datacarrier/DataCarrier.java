e7041571ee2e1b2588ef419222f4f3a994c813b4
Brendan Beagin <brendanpatrickbeagin@gmail.com>
2021-12-13 12:14:15 -0800

    Issues: Implemented #7

--- src/main/java/com/csun/RobotDevTeamWorld/sql/construction/datacarrier/DataCarrier.java
+++ src/main/java/com/csun/RobotDevTeamWorld/sql/construction/datacarrier/DataCarrier.java
@@ -1,19 +1,50 @@
 package com.csun.RobotDevTeamWorld.sql.construction.datacarrier;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
-import com.csun.RobotDevTeamWorld.sql.construction.SQLBuilder;
+import com.csun.RobotDevTeamWorld.sql.construction.SelectBuilder;
 
 public abstract class DataCarrier {
 	
 	private HashMap<String, Object> members;
+	protected static final String[] params = new String[0];
 	
 	public DataCarrier() {
 		this.members = new HashMap<String, Object>();
+		for(String s : this.params)
+			this.members.put(s, null);
 	}
 	
+	public abstract void populate(SelectBuilder from);
 	public abstract String toString();
-	public abstract void populate(SQLBuilder builder);
+	
+	public Iterator<Entry<String, Object>> getIter() {
+		return this.members.entrySet().iterator();
+	}
+	
+	public String getParams() {
+		String params = "( ";
+		Iterator<Entry<String,Object>> iter = this.members.entrySet().iterator();
+		while(iter.hasNext()) 
+			params += iter.next().getKey() + (iter.hasNext() ? ", " : " )");
+		return params;
+	}
+	
+	public int size() {
+		return this.members.size();
+	}
+	
+	public boolean isValid() {
+		Iterator<Entry<String,Object>> iter = this.members.entrySet().iterator();
+		while(iter.hasNext()) {
+			Entry<String, Object> e = iter.next();
+			if(e.getKey()==null||e.getKey().equals("")||e.getValue()==null)
+				return false;
+		}
+		return true;
+	}
 	
 	protected boolean addMember(String key, Object value, boolean shouldOverride) {
 		if(shouldOverride)
@@ -33,4 +64,5 @@ public abstract class DataCarrier {
 	protected boolean hasKey(String key) {
 		return this.members.containsKey(key);
 	}
+	
 }
